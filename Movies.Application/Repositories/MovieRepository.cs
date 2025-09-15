886d234225eec3c4254f00ff13f621ca68c74539
NICO <annicolas2@gmail.com>
2025-09-15 17:13:19 -0500

    Added Sort in Search Field

--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -108,7 +108,18 @@ public class MovieRepository : IMovieRepository
     {
         using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
 
-        var result = await connection.QueryAsync(new CommandDefinition("""
+        var orderClause = string.Empty;
+
+        if (options.SortField is not null)
+        {
+            var sortColumn = options.SortField == "year" ? "yearofrelease" : options.SortField;
+            orderClause = $"""
+                , m. {sortColumn}
+                order by m.{sortColumn} {(options.SortOrder == SortOrder.Ascending ? "asc" : "desc")}
+                """;
+        }
+        
+        var result = await connection.QueryAsync(new CommandDefinition($"""
             select m.*, 
                    string_agg(distinct g.name, ',') as genres,
                    round(avg(r.rating), 1) as rating,
@@ -120,7 +131,7 @@ public class MovieRepository : IMovieRepository
                 and myr.userid = @userId
             where (@title is null or m.title like ('%' || @title || '%'))
             and (@yearofrelease is null or m.yearofrelease = @yearofrelease)
-            group by id, userrating
+            group by id, userrating {orderClause}
             """, new
         {
             userId = options.UserId,
--- Movies.Application/obj/Debug/net9.0/Movies.Application.AssemblyInfo.cs
+++ Movies.Application/obj/Debug/net9.0/Movies.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49e3038d0d7a99710b28fab5eb0b6a5746117395")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+374a637dae1b89094cc299e6e3f00fe40c41bb2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
