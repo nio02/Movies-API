0e265ae4a1f355dfd14daf2cd846d2a1a3c004e7
NICO <annicolas2@gmail.com>
2025-09-16 11:52:14 -0500

    Added HATEOAS Feature to Get Endpoint

--- Movies.Application/obj/Debug/net9.0/Movies.Application.AssemblyInfo.cs
+++ Movies.Application/obj/Debug/net9.0/Movies.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886d234225eec3c4254f00ff13f621ca68c74539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cbea3e2802241383960cf5d8dbbee6773fdf02d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
